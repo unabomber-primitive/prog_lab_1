0e24eda483dd84c708ca850852d85bd3b3161771
unabomber-primitive <94494424+unabomber-primitive@users.noreply.github.com>
2021-12-01 00:44:49 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,43 +2,54 @@ import java.util.Arrays;
 import java.lang.Math;
 import java.util.Locale;
 import java.util.Random;
-import java.util.stream.LongStream;
+import java.util.stream.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-	    //Создаем одномерный массив c типа long и заполняем его нечётными числами от 7 до 21 включительно в порядке убывания.
-        long[] c = new long[8];
-        c[0] = 21;
-        for (int i = 1; i<8; i++) {
-            c[i] = c[i-1] - 2;
+    public static long[] generateDescendingArrayWithStep(int fromTop, int toBottom, int step) {
+        int length = (fromTop - toBottom)/step + 1;
+        long[] arr = new long[length];
+        arr[0] = fromTop;
+        for (int i = 1; i < length; i++) {
+            arr[i] = arr[i-1] - step;
         }
+        return(arr);
+    }
+
+    public static void main(String[] args) {
+
+        long[] c = generateDescendingArrayWithStep(21, 7, 2);
 
         //Создаем одномерный массив x типа double и заполняем его 12-ю случайными числами в диапазоне от -4.0 до 14.0.
         Random random = new Random();
         double[] x = random.doubles(12, -4.0, 14.0).toArray();
-
-        //Сздаем двумерный массив b с элементами класса String размером 8х12
-        String[][] b = new String[8][12];
-
+        
+        //Сздаем двумерный массив b с элементами класса Double размером 8х12
+        double[][] b = new double[8][12];
+        
         //Служебный массив
         long[] t = new long[] {13, 15, 17, 21};
-
+        
         //Вычисляем элементы массива b и заполняем его
-        for (int i = 0; i<8; i++) {
-            for (int j = 0; j < 12; j++) {
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < b[0].length; j++) {
                 long k = c[i];
-                if (c[i] == 9) {
-                    b[i][j] = String.format(Locale.ROOT,"%.4f", Math.pow(Math.E, Math.pow(Math.pow(Math.E, x[j]), 2)));
+                if (k == 9) {
+                    b[i][j] = Math.pow(Math.E, Math.pow(Math.pow(Math.E, x[j]), 2));
                 }
                 else if (LongStream.of(t).anyMatch(z -> z == k)) {
-                    b[i][j] = String.format(Locale.ROOT,"%.4f", Math.tan(Math.tan(Math.cos(x[j]))));
+                    b[i][j] = Math.tan(Math.tan(Math.cos(x[j])));
                 }
                 else {
-                    b[i][j] = String.format(Locale.ROOT,"%.4f", Math.asin(Math.sin(Math.cbrt(Math.log(Math.pow(Math.E, x[j]))))));
+                    b[i][j] = Math.asin(Math.sin(Math.cbrt(Math.log(Math.pow(Math.E, x[j])))));
                 }
             }
         }
-        System.out.println(Arrays.deepToString(b).replace("], ", "],\n"));
+        for (int i = 0; i < b.length; i++) {
+            System.out.print('[');
+            for (int j = 0; j < b[0].length; j++) {
+            	System.out.print(String.format(Locale.ROOT, j == b[0].length-1 ? "%.4f],\n" : "%.4f, ", b[i][j]));
+            }
+        }
     }
 }
